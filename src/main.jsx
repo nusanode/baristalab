6f5c637c5f94dff6db68214fe50cd8bb4404f94f
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-09 09:35:12 +0700

    Fix import path for LanguageProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import { LanguageProvider } from "./context/LanguageContext.jsx";
+import { LanguageProvider } from "./Context/LanguageContext.jsx";
 
 // Simpan elemen root ke dalam variabel
 const rootElement = document.getElementById("root");
