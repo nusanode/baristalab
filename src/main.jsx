e4c40563a5b382b3a2413d20ac12aecc62ab5b15
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-11 22:42:12 +0700

    Refactor main.jsx to simplify rendering
    Removed BrowserRouter and optimized the rendering process.

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-import { LanguageProvider } from "./Context/LanguageContext.jsx";
+import { LanguageProvider } from "./context/LanguageContext.jsx";
 
-// Simpan elemen root ke dalam variabel
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-
-      <BrowserRouter>
-
-        <LanguageProvider>
-          <App />
-        </LanguageProvider>
-
-      </BrowserRouter>
-
-    </StrictMode>,
-  );
-
-  // KODE OPTIMASI SEO
-  document.dispatchEvent(new Event('custom-render-trigger'));
-}
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  </StrictMode>,
+);
