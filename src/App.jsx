98ae52abd55d80a36427df97e256b53b692ad3a4
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-12 13:42:48 +0700

    Remove unused route components from App.jsx
    Removed multiple route components for various barista courses.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import { useState, useRef } from "react";
-import KursusBaristaJakarta from "./pages/KursusBaristaJakarta";
-import KursusBaristaTangerang from "./pages/KursusBaristaTangerang";
-import KursusBaristaBekasi from "./pages/KursusBaristaBekasi";
-import KursusBaristaDepok from "./pages/KursusBaristaDepok";
-import KursusBaristaBogor from "./pages/KursusBaristaBogor";
-import SertifikasiBarista from "./pages/SertifikasiBarista";
-import PelatihanBarista from "./pages/PelatihanBarista";
-import KelasLatteArt from "./pages/KelasLatteArt";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
@@ -79,54 +71,6 @@ function App() {
       <ScrollToTop />
     </div>
   );
-  <Route
-path="/kursus-barista-jakarta"
-element={<KursusBaristaJakarta />}
-/>
-  
-<Route
-path="/kursus-barista-tangerang"
-element={<KursusBaristaTangerang />}
-/>
-
-
-<Route
-path="/kursus-barista-bekasi"
-element={<KursusBaristaBekasi />}
-/>
-
-
-<Route
-path="/kursus-barista-depok"
-element={<KursusBaristaDepok />}
-/>
-
-
-<Route
-path="/kursus-barista-bogor"
-element={<KursusBaristaBogor />}
-/>
- 
-
-<Route
-path="/sertifikasi-barista"
-element={<SertifikasiBarista />}
-/>
-
-
-<Route
-path="/kelas-latte-art"
-element={<KelasLatteArt />}
-/>
-   <Route
-path="/pelatihan-barista"
-element={<PelatihanBarista />}
-/>
-
-
-</Routes>
-
-);  
 }
 
 export default App;
