ff3066fea1a8b7a98621179b968238e18425d0eb
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-14 21:09:35 +0700

    Refactor main.jsx to include BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    import { BrowserRouter } from "react-router-dom";
     <BrowserRouter>
-  <LanguageProvider>
-    <App />
-  </LanguageProvider>
-</BrowserRouter>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
