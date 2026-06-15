a79bd54aa1263174c291b2100cd5000b585cba54
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-15 17:54:29 +0700

    Add BrowserRouter for routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-<StrictMode>
-<LanguageProvider>
-<App />
-</LanguageProvider>
-</StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
