b7d94038efaa0217db93f054177538e50bfb2a19
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-16 10:59:57 +0700

    Implement routing with Home, KursusBarista, and KelasLatteArt
    Refactor App component to use React Router for navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import "./index.css";
-import App from "./App";
-import { LanguageProvider } from "./context/LanguageContext";
+import Home from "./pages/Home";
+import KursusBarista from "./pages/KursusBarista";
+import KelasLatteArt from "./pages/KelasLatteArt";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </BrowserRouter>
-  </StrictMode>
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route
+        path="/kursus-barista"
+        element={<KursusBarista />}
+      />
+
+      <Route
+        path="/kelas-latte-art"
+        element={<KelasLatteArt />}
+      />
+    </Routes>
+  );
+}
+
+export default App;
