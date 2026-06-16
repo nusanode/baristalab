794bc5d83aa7cd9f927e4200f9bbecf3a2278242
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-16 10:46:53 +0700

    Refactor route definitions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,15 @@ import KursusBarista from "./pages/KursusBarista";
 import KelasLatteArt from "./pages/KelasLatteArt";
 
 function App() {
-return (
-<Routes>
-<Route path="/" element={<Home />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-<Route  
-    path="/kursus-barista"  
-    element={<KursusBarista />}  
-  />  
+      <Route path="/kursus-barista" element={<KursusBarista />} />
 
-  <Route  
-    path="/kelas-latte-art"  
-    element={<KelasLatteArt />}  
-  />  
-</Routes>
-
-);
+      <Route path="/kelas-latte-art" element={<KelasLatteArt />} />
+    </Routes>
+  );
 }
 
 export default App;
