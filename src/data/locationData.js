a75f11d0e2aceb73ccefde91ea4d6b176ce4d047
csprivakom-ctrl <cs.privakom@gmail.com>
2026-06-16 21:31:08 +0700

    Enhance location data and descriptions for barista courses
    Updated location details and descriptions for various barista courses in Jakarta, Tangerang, Jakarta Timur, and added new courses in Bekasi, Depok, and Bogor.

--- src/data/locationData.js
+++ src/data/locationData.js
@@ -2,7 +2,7 @@ export const locationData = {
   "kursus-barista-jakarta": {
     title: "Kursus Barista Jakarta",
     h1: "Kursus Barista Jakarta Profesional",
-    location: "Jakarta",
+    location: "Jakarta, Jakarta Timur, Barat, Selatan, Utara, & Pusat",
     phone: "6285213541993",
     maps: "https://maps.app.goo.gl/WFVcwu185iuZZcdp7",
     description:
@@ -12,21 +12,51 @@ export const locationData = {
   "kursus-barista-tangerang": {
     title: "Kursus Barista Tangerang",
     h1: "Kursus Barista Tangerang Profesional",
-    location: "Tangerang",
+    location: "Tangerang, Tangerang Selatan, BSD, Gading Serpong, & Karawaci",
     phone: "6281356561721",
     maps: "https://maps.app.goo.gl/ZtByeSPviSn4f1cj9",
     description:
-      "Program pelatihan barista Tangerang dengan praktik langsung menggunakan mesin espresso komersial."
+      "Program pelatihan barista Tangerang dengan praktik langsung menggunakan mesin espresso komersial. 1 orang 1 mesin komersial, bersertifikat resmi!"
   },
 
   "kursus-barista-jakarta-timur": {
     title: "Kursus Barista Jakarta Timur",
     h1: "Kursus Barista Jakarta Timur Profesional",
-    location: "Duren Sawit Jakarta Timur",
+    location: "Duren Sawit, Buaran, Klender, Pondok Kopi, & Jatinegara Jakarta Timur",
     phone: "6285213541993",
     maps: "https://maps.app.goo.gl/WFVcwu185iuZZcdp7",
     description:
-      "Pelatihan barista Jakarta Timur untuk pemula, profesional dan calon pemilik coffee shop."
+      "Pelatihan barista Jakarta Timur untuk pemula, profesional dan calon pemilik coffee shop. Laboratorium praktik privat di Duren Sawit."
+  },
+
+  "kursus-barista-bekasi": {
+    title: "Kursus Barista Bekasi",
+    h1: "Kursus Barista Bekasi Profesional",
+    location: "Bekasi Kota, Kabupaten Bekasi, Cikarang, Tambun, & Babelan",
+    phone: "6285213541993", // diarahkan ke cabang terdekat (Jaktim)
+    maps: "https://maps.app.goo.gl/WFVcwu185iuZZcdp7",
+    description:
+      "Daftar kursus barista profesional di Bekasi, Cikarang, Tambun, & Babelan. Pelatihan espresso, latte art, dan coffee business. Bersertifikat resmi!"
+  },
+
+  "kursus-barista-depok": {
+    title: "Kursus Barista Depok",
+    h1: "Kursus Barista Depok Profesional",
+    location: "Kota Depok, Margonda, Cinere, Sawangan, & Cimanggis",
+    phone: "6285213541993", // diarahkan ke cabang terdekat (Jaktim)
+    maps: "https://maps.app.goo.gl/WFVcwu185iuZZcdp7",
+    description:
+      "Daftar kursus barista profesional di Depok, Margonda, Cinere, & Sawangan. Pelatihan espresso, latte art, dan coffee business. 1 orang 1 mesin!"
+  },
+
+  "kursus-barista-bogor": {
+    title: "Kursus Barista Bogor",
+    h1: "Kursus Barista Bogor Profesional",
+    location: "Bogor Kota, Sentul, Cibinong, Bojonggede, Ciawi, & Parung",
+    phone: "6281356561721", // diarahkan ke cabang terdekat (Tangerang)
+    maps: "https://maps.app.goo.gl/ZtByeSPviSn4f1cj9",
+    description:
+      "Daftar kursus barista profesional di Bogor, Sentul, Cibinong, & Ciawi. Pelatihan espresso, latte art, dan coffee business. Bersertifikat resmi!"
   },
 
   "pelatihan-barista-jabodetabek": {
@@ -39,3 +69,4 @@ export const locationData = {
       "Program pelatihan barista Jabodetabek dengan kurikulum industri dan trainer berpengalaman."
   }
 };
+
